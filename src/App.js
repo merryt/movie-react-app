045aa64ec56641c425370245dd0b71c01c2c53e6
merryt <tymerry@gmail.com>
2022-05-26 15:10:49 +0200

    playing with props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const Intro = ({ user = { name: "guy" } }) => {
+
+  return (
+    <div>navigation, welcome back {user.name}</div>
+  )
+}
+
+
+const App = () => {
+  const user = {
+    name: "tyler",
+    email: "test@test.com"
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Intro user={user} />
+        <Intro />
+        <p>hi</p>
       </header>
     </div>
   );
