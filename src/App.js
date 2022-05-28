ceced07da504f1fc2d30001037590e4a459cc053
merryt <tymerry@gmail.com>
2022-05-28 15:43:58 +0200

    removing old components to get ready for movie stuff

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import './App.css';
 import React, { PropTypes } from 'react';
-import Counter from './Counter';
-import { Accordian } from './Accordian';
-import ControlledInput from './ControlledInput';
-
-
-const Intro = ({ user = { name: "guy" } }) => {
-
-  return (
-    <div>Navigation, welcome back {user.name}</div>
-  )
-}
 
 
 const App = () => {
-
-  const user = {
-    name: "tyler",
-    email: "test@test.com"
-  }
-
-  let intro = <h1>this is an example of html as a variable</h1>
-
   return (
     <div className="App">
       <header className="App-header">
-        {intro}
-        <Intro user={user} />
-        <Intro />
-        <p>hi</p>
-        <Counter />
-        <Accordian />
-        <ControlledInput />
       </header>
     </div>
   );
